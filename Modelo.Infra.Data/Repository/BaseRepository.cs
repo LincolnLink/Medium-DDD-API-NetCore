a06dd0316dbed4bffd5603954b0b23112000cfc7
Lincoln Ferreira Campos <link7-7@hotmail.com>
2019-10-14 05:09:23 -0300

    implementando a interface de repositorio na camada de infra

--- Modelo.Infra.Data/Repository/BaseRepository.cs
+++ Modelo.Infra.Data/Repository/BaseRepository.cs
@@ -8,36 +8,48 @@ using System.Text;
 
 namespace Modelo.Infra.Data.Repository
 {
-    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    public class BaseRepository<T> : IRepository<User> where T : BaseEntity
     {
-        private MySqlContext context = new MySqlContext();
+        //private MySqlContext context = new MySqlContext();
 
-        public void Insert(T obj)
+        private readonly MySqlContext _contexto;
+
+        public BaseRepository(MySqlContext ctx)
         {
-            context.Set<T>().Add(obj);
-            context.SaveChanges();
+            _contexto = ctx;
         }
 
-        public void Update(T obj)
+        public void Insert(User usuario)
         {
-            context.Entry(obj).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-            context.SaveChanges();
+            _contexto.User.Add(usuario);
+            _contexto.SaveChanges();
+        }
+
+        public void Update(User usuario)
+        {
+            _contexto.User.Update(usuario);
+            _contexto.SaveChanges();
         }        
 
-        public T Select(int id)
+        public User Select(int id)
         {
-            return context.Set<T>().Find(id);
+            return _contexto.User.FirstOrDefault(u => u.Id == id);
         }
 
         public void Remove(int id)
         {
-            context.Set<T>().Remove(Select(id));
-            context.SaveChanges();
+            var entity = _contexto.User.First(u => u.Id == id);
+
+            if (entity != null)
+            {
+                _contexto.User.Remove(entity);
+                _contexto.SaveChanges();
+            }
         }
 
-        public IList<T> SelectAll()
+        public IList<User> SelectAll()
         {
-            return context.Set<T>().ToList();
+            return _contexto.User.ToList();
         }
     }
 }
